bf0cc652481db87367279fdf055dbaf5736e40c8
dev.edugzprm <dev.edugzprm@yandex.ru>
2025-05-22 01:34:49 +0700

    feat: add styles to class feat: JS to change active slide style: use Prettier linter

---
+++ index.js
@@ -0,0 +1,20 @@
+function slidePlugin(activeSlide = 2) {
+  const slides = document.querySelectorAll(".slide");
+
+  slides[activeSlide].classList.add("active");
+
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+      slide.classList.add("active");
+    });
+  }
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  }
+}
+
+slidePlugin();
